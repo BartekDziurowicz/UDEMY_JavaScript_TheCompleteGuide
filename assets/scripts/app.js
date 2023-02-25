338620644308eedb00c25d091b201ddf3b8b05e2
BartekDziurowicz <bartosz.dziurowicz@gmail.com>
2023-02-25 14:30:02 +0100

    Arrays and Objects

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,36 +1,51 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
+let logEntries = [];
 
 const getUserNumberInput = () => {
     return parseInt(userInput.value);
 }
 
+const createAndWriteOutput = (operator, resultBeforeCalc, calcNumber) => {
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+    outputResult(currentResult, calcDescription);
+}
+
 const add = () => {
     const enteredNumber = getUserNumberInput();
-    const calcDescription = `${currentResult} + ${enteredNumber}`;
+    const initialResult = currentResult;
     currentResult += enteredNumber;   
-    outputResult(currentResult, calcDescription);
+    createAndWriteOutput('+', initialResult, enteredNumber);
+    const logEntry = {
+        operation: 'ADD',
+        prevResult: initialResult,
+        number: enteredNumber,
+        result: currentResult
+    };
+    logEntries.push(logEntry);
+    console.log(logEntry.operation);
+    console.log(logEntries);
 }
 
 const substract = () => {
     const enteredNumber = getUserNumberInput();
-    const calcDescription = `${currentResult} - ${enteredNumber}`;
+    const initialResult = currentResult;
     currentResult -= enteredNumber;   
-    outputResult(currentResult, calcDescription);
+    createAndWriteOutput('-', initialResult, enteredNumber);
 }
 
 const multiply = () => {
     const enteredNumber = getUserNumberInput();
-    const calcDescription = `${currentResult} * ${enteredNumber}`;
+    const initialResult = currentResult;;
     currentResult *= enteredNumber;   
-    outputResult(currentResult, calcDescription);
+    createAndWriteOutput('*', initialResult, enteredNumber);
 }
 
 const divide = () => {
     const enteredNumber = getUserNumberInput();
-    const calcDescription = `${currentResult} / ${enteredNumber}`;
+    const initialResult = currentResult;
     currentResult /= enteredNumber;   
-    outputResult(currentResult, calcDescription);
+    createAndWriteOutput('/', initialResult, enteredNumber);
 }
 
 addBtn.addEventListener('click', add);
