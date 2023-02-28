036545d88b57a4c5f4dc96aa77f6024e604d904f
BartekDziurowicz <bartosz.dziurowicz@gmail.com>
2023-02-28 19:26:51 +0100

    Adding more if statements and a strong attack functionality

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,5 @@
 const ATTACK_VALUE = 10;
+const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
 
 let chosenMaxLife = 100;
@@ -7,18 +8,34 @@ let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
-function attackHandler() {
-  const damage = dealMonsterDamage(ATTACK_VALUE);
+function attackMonster(mode) {
+  let maxDamage;
+  if (mode === 'ATTACK') {
+    maxDamage = ATTACK_VALUE;
+  } else if (mode === 'STRONG_ATTACK') {
+    maxDamage = STRONG_ATTACK_VALUE;
+  }
+  const damage = dealMonsterDamage(maxDamage);
   currentMonsterHealth -= damage;
   const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
   currentPlayerHealth -= playerDamage;
   if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
-    alert('You won!');
+    alert("You won!");
   } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
-    alert('You lost!');
+    alert("You lost!");
   } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
-    alert('You have a draw!');
+    alert("You have a draw!");
   }
 }
 
-attackBtn.addEventListener('click', attackHandler);
+function attackHandler() {
+  attackMonster('ATTACK');
+}
+
+attackBtn.addEventListener("click", attackHandler);
+
+function strongAttackHandler() {
+  attackMonster('STRONG_ATTACK');
+}
+
+strongAttackBtn.addEventListener("click", strongAttackHandler);
