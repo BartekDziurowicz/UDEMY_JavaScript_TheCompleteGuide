1a4e39363cda33444d3cf1984f1d8c47e74e38d3
BartekDziurowicz <bartosz.dziurowicz@gmail.com>
2023-03-05 18:09:27 +0100

    implementic the core game logic

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const ROCK = "ROCK";
 const PAPER = "PAPER";
 const SCISSORS = "SCISSORS";
 const DEFAULT = ROCK;
+const RESULT_DRAW = "DRAW";
+const RESULT_PLAYER_WINS = "PLAYER WINS";
+const RESULT_COMPUTER_WINS = "COMPUTER_WINS";
 
 let gamseIsRunning = false;
 
@@ -16,6 +19,31 @@ const getPlayerChoice = function () {
   return selection;
 };
 
+const getComputerChoice = function () {
+  const randomValue = Math.random();
+  if (randomValue < 0.34) {
+    return ROCK;
+  } else if (randomValue < 0.67) {
+    return PAPER;
+  } else {
+    return SCISSORS;
+  }
+};
+
+const getWinner = function (cChoice, pChoice) {
+  if (cChoice === pChoice) {
+    return RESULT_DRAW;
+  } else if (
+    (cChoice === ROCK && pChoice === PAPER) ||
+    (cChoice === SCISSORS && pChoice === ROCK) ||
+    (cChoice === PAPER && pChoice === SCISSORS)
+  ) {
+    return RESULT_PLAYER_WINS;
+  } else {
+    return RESULT_COMPUTER_WINS;
+  }
+};
+
 startGameBtn.addEventListener("click", function () {
   if (gamseIsRunning) {
     return;
@@ -23,4 +51,6 @@ startGameBtn.addEventListener("click", function () {
   gamseIsRunning = true;
   console.log("Game is starting...");
   const playerSelection = getPlayerChoice();
+  const computerChoice = getComputerChoice();
+  const winner = getWinner(computerChoice, playerSelection);
 });
