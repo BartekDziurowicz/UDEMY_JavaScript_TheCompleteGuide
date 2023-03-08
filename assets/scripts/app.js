3bd99d823f973b079979a302d34fcf1b0877883f
BartekDziurowicz <bartosz.dziurowicz@gmail.com>
2023-03-08 18:07:45 +0100

    selecting the modal and ADD button

---
+++ assets/scripts/app.js
@@ -0,0 +1,9 @@
+const addMovieModal = document.getElementById('add-modal');
+// other approachs:
+// const addMovieModal = document.querySelector('#add-modal');
+// const addMovieModal = document.body.children[1];
+
+const startAddMovieButton = document.querySelector('header button');
+// other approachs:
+// const startAddMovieButton = document.header.child[1];
+// const startAddMovieButton = document.querySelector('header').lastElementChild;
