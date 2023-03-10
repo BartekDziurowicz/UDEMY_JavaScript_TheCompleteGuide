bf50a03e79b94f2944982b5632f126d7d8e7b565
BartekDziurowicz <bartosz.dziurowicz@gmail.com>
2023-03-10 05:00:21 +0100

    fetching and validating user input

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,34 +1,53 @@
-const addMovieModal = document.getElementById('add-modal');
+const addMovieModal = document.getElementById("add-modal");
 // other approachs:
 // const addMovieModal = document.querySelector('#add-modal');
 // const addMovieModal = document.body.children[1];
 
-const startAddMovieButton = document.querySelector('header button');
+const startAddMovieButton = document.querySelector("header button");
 // other approachs:
 // const startAddMovieButton = document.header.child[1];
 // const startAddMovieButton = document.querySelector('header').lastElementChild;
 
-const backdrop = document.getElementById('backdrop');
-const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
+const backdrop = document.getElementById("backdrop");
+const cancelAddMovieButton = addMovieModal.querySelector(".btn--passive");
+const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
+const userInputs = addMovieModal.querySelectorAll("input");
+// const userInputs = addMovieModal.getElementsByTagName('input');
 
 const toggleBackdrop = () => {
-    backdrop.classList.toggle('visible');
-}
+  backdrop.classList.toggle("visible");
+};
 
 const toggleMovieModal = () => {
-    addMovieModal.classList.toggle('visible');
-    toggleBackdrop();
-}
+  addMovieModal.classList.toggle("visible");
+  toggleBackdrop();
+};
 // function toogleMovieModal () {}; or anonymous function can be used as well
 
-const cancelAddMovie = () => {
-    toggleMovieModal();
-}
+const cancelAddMovieHandler = () => {
+  toggleMovieModal();
+};
+
+const addMovieHandler = () => {
+  const titleValue = userInputs[0].value;
+  const imageUrlValue = userInputs[1].value;
+  const ratingValue = userInputs[2].value;
+  if (
+    titleValue.trim() === "" ||
+    imageUrlValue.trim() === "" ||
+    ratingValue.trim() === "" ||
+    ratingValue < 1 ||
+    ratingValue > 5
+  ) {
+    alert('Please enter valid values.');
+  }
+};
 
 const backdropClickHandler = () => {
-    toggleMovieModal();
-}
+  toggleMovieModal();
+};
 
-startAddMovieButton.addEventListener('click', toggleMovieModal);
-backdrop.addEventListener('click', backdropClickHandler);
-cancelAddMovieButton.addEventListener('click', cancelAddMovie);
+startAddMovieButton.addEventListener("click", toggleMovieModal);
+backdrop.addEventListener("click", backdropClickHandler);
+cancelAddMovieButton.addEventListener("click", cancelAddMovieHandler);
+confirmAddMovieButton.addEventListener("click", addMovieHandler);
