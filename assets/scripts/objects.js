ebe78f81fb8d346b75238e42670523a6017bfe82
BartekDziurowicz <bartosz.dziurowicz@gmail.com>
2023-03-20 17:31:01 +0100

    rendering elements based on objects

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -3,23 +3,47 @@ const searchBtn = document.getElementById('search-btn');
 
 const movies = [];
 
+const renderMovies = () => {
+  const movieList = document.getElementById('movie-list');
+
+  if (movies.length === 0) {
+    movieList.classList.remove('visible');
+    return;
+  } else {
+    movieList.classList.add('visible');
+  }
+  movieList.innerHTML = '';
+
+  movies.forEach((movie) => {
+    const movieEl = document.createElement('li');
+    movieEl.textContent = movie.info.title;
+    movieList.append(movieEl);
+  });
+};
+
 const addMovieHandler = () => {
-    const title = cocument.getElementById('title').value;
-    const extraName = document.getElementById('extra-name').value;
-    const extraValue = document.getElementById('extra-value').value;
+  const title = document.getElementById('title').value;
+  const extraName = document.getElementById('extra-name').value;
+  const extraValue = document.getElementById('extra-value').value;
 
-    if (title.trim ==='' || extraName.trim === '' || extraValue.trim === '') {
-        return;
-    }
+  if (
+    title.trim() === '' ||
+    extraName.trim() === '' ||
+    extraValue.trim() === ''
+  ) {
+    return;
+  }
 
-    const newMovie = {
-        info: {
-            title,
-            [extraName]: extraValue,
-        },
-        id: Math.random()
-    };
-    movies.push(newMovie);
+  const newMovie = {
+    info: {
+      title,
+      [extraName]: extraValue
+    },
+    id: Math.random()
+  };
+
+  movies.push(newMovie);
+  renderMovies();
 };
 
 addMovieBtn.addEventListener('click', addMovieHandler);
