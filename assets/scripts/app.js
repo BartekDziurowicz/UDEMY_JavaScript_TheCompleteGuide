a05fa4320f4ae5a2e59737ed50c90b9564069af3
BartekDziurowicz <bartosz.dziurowicz@gmail.com>
2023-03-26 15:33:44 +0200

    static methods and properties

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -17,7 +17,7 @@ class ShoppingCart {
 
   addProduct(product) {
     this.items.push(product);
-    this.totalOutput = `<h2>Total: \$${1}</h2>`;
+    this.totalOutput.innerHTML = `<h2>Total: \$${1}</h2>`;
   }
 
   render() {
@@ -27,7 +27,7 @@ class ShoppingCart {
       <button>Order Now!</button>
       `;
     cartEl.className = "cart";
-    this.totalOutput = cartEl.querySelector('h2');
+    this.totalOutput = cartEl.querySelector("h2");
     return cartEl;
   }
 }
@@ -38,9 +38,10 @@ class ProductItem {
   }
 
   addToCart() {
-    console.log("Adding product to cart...");
-    console.log(this.product);
-    cart.addProduct();
+    // console.log("Adding product to cart...");
+    // console.log(this.product);
+    // cart.addProduct();
+    App.addProductToCart(this.product)
   }
 
   render() {
@@ -96,8 +97,8 @@ class ProductList {
 class Shop {
   render() {
     const renderHook = document.getElementById("app");
-    const cart = new ShoppingCart();
-    const cartEl = cart.render();
+    this.cart = new ShoppingCart();
+    const cartEl = this.cart.render();
     const producList = new ProductList();
     const prodListEl = producList.render();
 
@@ -106,5 +107,16 @@ class Shop {
   }
 }
 
-const shop = new Shop();
-shop.render();
+class App {
+  static init() {
+    const shop = new Shop();
+    shop.render();
+    this.cart = shop.cart;
+  }
+
+  static addProductToCart(product) {
+    this.cart.addProduct(product);
+  }
+}
+
+App.init();
