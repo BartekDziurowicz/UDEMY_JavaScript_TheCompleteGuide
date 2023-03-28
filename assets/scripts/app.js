dff0312dad996d695172a1408ea301715132013c
BartekDziurowicz <bartosz.dziurowicz@gmail.com>
2023-03-28 20:20:20 +0200

    starting with the swithc project

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,22 +1,43 @@
-class Tooltip {
-
-}
+class Tooltip {}
 
 class ProjectItem {
+  constructor(id) {
+    this.id = id;
+    this.connectMoreInfoButton();
+    this.connectSwitchButton();
+  }
 
+  connectMoreInfoButton() {}
+
+  connectSwitchButton() {
+    const projectItemElement = document.getElementById(this.id);
+    const switchBtn = projectItemElement.querySelector("button:last-of-type");
+    switchBtn.addEventListener("click");
+  }
 }
 
 class ProjectList {
-    constructor(type) {
-        const prjItems = document.querySelectorAll(`#${type}-projects li`);
+  projects = [];
+  constructor(type) {
+    const prjItems = document.querySelectorAll(`#${type}-projects li`);
+    for (const prjItem of prjitems) {
+      this.projects.push(new ProjectItem(prjItem.id));
     }
+  }
+
+  addProject() {}
+
+  switchProject(projectId) {
+    const projectIndex = this.projects.findIndex(p => p.id === projectId);
+    this.projects.splice(projectIndex, 1);
+  }
 }
 
 class App {
-    static init() {
-        const activeProjectList = new ProjectList('active');
-        const finishedProjectsList = new ProjectList('finished');
-    }
+  static init() {
+    const activeProjectList = new ProjectList("active");
+    const finishedProjectsList = new ProjectList("finished");
+  }
 }
 
 App.init();
