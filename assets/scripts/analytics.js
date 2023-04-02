9f4175bf7a0926df758da74759166c61fffa1e50
BartekDziurowicz <bartosz.dziurowicz@gmail.com>
2023-04-02 19:49:52 +0200

    setting timers and intervals

--- assets/scripts/analytics.js
+++ assets/scripts/analytics.js
@@ -1,1 +1,11 @@
-console.log('sending analytics...');
+const intervalId = setInterval(
+  () => {
+    console.log("sending analytics data...");
+  },
+  2000,
+  []
+); //after evry 2s function will be called
+
+document.getElementById("stop-analytics-btn").addEventListener("click", () => {
+  clearInterval(intervalId);
+});
