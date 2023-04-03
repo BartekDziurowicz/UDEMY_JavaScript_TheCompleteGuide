827b622a2b136a520e1798281effc32021668c16
BartekDziurowicz <bartosz.dziurowicz@gmail.com>
2023-04-03 16:36:58 +0200

    working with preventDefault

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -4,19 +4,26 @@ const button = document.querySelector('button');
 //     alert('alert');
 // }
 
-const buttonClickHandler = event => {
-    event.target.disabled = true;
+// const buttonClickHandler = event => {
+//     event.target.disabled = true;
+//     console.log(event);
+// }
+
+// const anotherButtonClickHandler = () => {
+//     console.log('this was clicked');
+// }
+
+// // button.onclick = buttonClickHandler;
+
+// button.addEventListener('click', buttonClickHandler);
+
+// setTimeout(() => {
+//     button.removeEventListener('click', buttonClickHandler);
+// }, 2000);
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', event => {
+    event.preventDefault(); // prevent default behaviour, now page is not reloaded after submit button is clicked
     console.log(event);
-}
-
-const anotherButtonClickHandler = () => {
-    console.log('this was clicked');
-}
-
-// button.onclick = buttonClickHandler;
-
-button.addEventListener('click', buttonClickHandler);
-
-setTimeout(() => {
-    button.removeEventListener('click', buttonClickHandler);
-}, 2000);
+})
