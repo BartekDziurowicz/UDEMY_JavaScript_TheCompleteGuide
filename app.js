3fb42770693c2bcf82e2bc367bb6b9ca8a57175d
BartekDziurowicz <bartosz.dziurowicz@gmail.com>
2023-04-04 18:01:32 +0200

    factory functions

--- app.js
+++ app.js
@@ -15,3 +15,19 @@ let n = 0;
 function withSideEffect(num1) {
     return num1 + n;
 }
+
+
+// factory function - produce another function
+function createTaxCalculator(tax) {
+    function calculateTax(amount) {
+        return amount*tax;
+    }
+
+    return calculateTax; //return pointer to inner function
+}
+
+const calculateVatAmount = createTaxCalculator(0.19);
+const calculateIncomeTaxAmount = createTaxCalculator(0.25);
+
+console.log(calculateVatAmount(100));
+console.log(calculateIncomeTaxAmount(200));
