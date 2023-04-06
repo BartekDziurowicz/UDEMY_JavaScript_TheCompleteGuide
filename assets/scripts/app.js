46ce557418bcaf7427124e7a327dd84648c1b6cc
BartekDziurowicz <bartosz.dziurowicz@gmail.com>
2023-04-06 17:17:39 +0200

    promysifaing http request with then

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,29 +1,31 @@
-const listElement = document.querySelector('.posts');
-const postTemplate = document.getElementById('single-post')
+const listElement = document.querySelector(".posts");
+const postTemplate = document.getElementById("single-post");
 
-const xhr = new XMLHttpRequest();
+// promysifing
+function sendHttpRequest(method, url) {
+  const promise = new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.responseType = "json";
 
-xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
+    xhr.onload = function () {
+      resolve(xhr.response);
+    };
+    xhr.send();
+  });
+  return primise;
+}
 
-// esponse shoud be parsed into javasript object from json
-
-// first approach
-// xhr.onload = function() {
-//     const listOfPost = JSON.parse(xhr.response);
-// }
-
-// second approach
-xhr.responseType = 'json';
-
-xhr.onload = function() {
-    const listOfPosts = xhr.response;
-    for (const post of listOfPosts) {
-      const postEl = document.importNode(postTemplate.content, true);
-      postEl.querySelector('h2').textContent = post.title.toUpperCase();
-      postEl.querySelector('p').textContent = post.body;
-      listElement.append(postEl);
-    }
-  };
-
-
-xhr.send();
+function fetchPosts() {
+    const responseData = sentHttpRequest('GET', 'https://jsonplaceholder.typicode.com/posts').then(
+        responseData => {
+            const listOfPosts = xhr.response;
+            for (const post of listOfPosts) {
+              const postEl = document.importNode(postTemplate.content, true);
+              postEl.querySelector("h2").textContent = post.title.toUpperCase();
+              postEl.querySelector("p").textContent = post.body;
+              listElement.append(postEl);
+            }
+        }
+    )
+}
