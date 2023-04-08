3bb77513e494b5310394a1dd3583e7d97215d777
BartekDziurowicz <bartosz.dziurowicz@gmail.com>
2023-04-08 18:08:22 +0200

    using fetch API

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,27 +6,30 @@ const postList = document.querySelector('ul');
 
 // promysifing
 function sendHttpRequest(method, url, data) {
-  const promise = new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open(method, url);
-    xhr.responseType = "json";
+  // const promise = new Promise((resolve, reject) => {
+    // const xhr = new XMLHttpRequest();
+    // xhr.open(method, url);
+    // xhr.responseType = "json";
 
-    xhr.onload = function () {
-      if (xhr.status >= 200 && xhr.status < 300) {
-        resolve(xhr.response);
-      } else {
-        reject(new Error('Something went wrong'));
-      }
+    // xhr.onload = function () {
+    //   if (xhr.status >= 200 && xhr.status < 300) {
+    //     resolve(xhr.response);
+    //   } else {
+    //     reject(new Error('Something went wrong'));
+    //   }
       
-    };
+    // };
 
-    xhr.onerror = function() {
-      reject(new Error('Failed to send request'));
-    }
+    // xhr.onerror = function() {
+    //   reject(new Error('Failed to send request'));
+    // }
 
-    xhr.send(JSON.stringify(data));
-  });
-  return promise;
+    // xhr.send(JSON.stringify(data));
+  // });
+  // return promise;
+  return fetch(url).then( response => {
+    return response.json();
+  } );
 }
 
 async function fetchPosts() {
