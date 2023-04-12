9fd1121b8ead964b42b4190f38fe13e8dd93fbbf
BartekDziurowicz <bartosz.dziurowicz@gmail.com>
2023-04-12 18:24:25 +0200

    generators

--- app.js
+++ app.js
@@ -1,30 +1,71 @@
 // Library land
-const uid = Symbol('uid');
+const uid = Symbol();
 console.log(uid);
 
-const person = {
-    [uid]: 'p1',
-    name: 'Bart',
-    age: 30,
-    [Symbol.toStringTag]: 'User uid'
+const user = {
+  // id: 'p1',
+  [uid]: 'p1',
+  name: 'Max',
+  age: 30,
+  [Symbol.toStringTag]: 'User'
 };
 
-// app land -> using the Library
-user.id = 'p2'; // this should be not possible
+user[uid] = 'p3';
 
-console.log(user);
+// app land => Using the library
 
+user.id = 'p2'; // this should not be possible!
 
-// iterators
+console.log(user[Symbol('uid')]);
+console.log(Symbol('uid') === Symbol('uid'));
+console.log(user.toString());
+
 const company = {
-    employees: ['Max', 'Manu', 'Anna'],
-    next() {
-        if (this.curEmployee >= this.employees.length) {
-            return { value: this.curEmployee, done: true };
-        }
-        const returnValue =  { value: this.employees[this.curEmployee], done: false };
-        this.curEmployee++;
-        return returnValue;
+  // curEmployee: 0,
+  employees: ['Max', 'Manu', 'Anna'],
+  // next() {
+  //   if (this.curEmployee >= this.employees.length) {
+  //     return { value: this.curEmployee, done: true };
+  //   }
+  //   const returnValue = {
+  //     value: this.employees[this.curEmployee],
+  //     done: false
+  //   };
+  //   this.curEmployee++;
+  //   return returnValue;
+  // },
+  [Symbol.iterator]: function* employeeGenerator() {
+    // let employee = company.next();
+
+    // while(!employee.done) {
+    //   yield employee.value;
+    //   employee = company.next();
+    // }
+    let currentEmployee = 0;
+    while(currentEmployee < this.employees.length) {
+      yield this.employees[currentEmployee];
+      currentEmployee++;
     }
+  }
+};
+
+// let employee = company.next();
+
+// while(!employee.done) {
+//   console.log(employee.value);
+//   employee = company.next();
+// }
+
+for (const employee of company) {
+  console.log(employee);
 }
 
+console.log([...company]);
+
+// const it = company.getEmployee();
+
+// console.log(it.next());
+// console.log(it.next());
+// console.log(it.next());
+// console.log(it.next());
+// console.log(it.next());
