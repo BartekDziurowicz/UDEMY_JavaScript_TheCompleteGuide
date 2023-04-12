02f33f2567bc52c9f0129d550663045268633151
BartekDziurowicz <bartosz.dziurowicz@gmail.com>
2023-04-12 18:15:41 +0200

    understanding iterators

--- app.js
+++ app.js
@@ -14,3 +14,17 @@ user.id = 'p2'; // this should be not possible
 
 console.log(user);
 
+
+// iterators
+const company = {
+    employees: ['Max', 'Manu', 'Anna'],
+    next() {
+        if (this.curEmployee >= this.employees.length) {
+            return { value: this.curEmployee, done: true };
+        }
+        const returnValue =  { value: this.employees[this.curEmployee], done: false };
+        this.curEmployee++;
+        return returnValue;
+    }
+}
+
