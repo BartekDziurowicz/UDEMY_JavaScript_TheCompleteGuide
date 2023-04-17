575624d00eb0657595388a21b6a01a1cbc8b39f8
BartekDziurowicz <bartosz.dziurowicz@gmail.com>
2023-04-17 16:16:32 +0200

    listening to events inside the component

--- tooltip.js
+++ tooltip.js
@@ -1,12 +1,25 @@
 class Tooltip extends HTMLElement {
     constructor() {
         super();
+        this._tooltipContainer;
     }
 
     connectedCallback() {
         const tooltipIcon = document.createElement('span');
-        tooltipIcon.textContent = ' (?)'
+        tooltipIcon.textContent = ' (?)';
+        tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
+        tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
         this.appendChild(tooltipIcon);
+    }
+
+    _showTooltip() {
+        this.tooltipContainer = document.createElement('div');
+        tooltipContainer.textContent = 'This is the tooltip text!';
+        this.appendChild(this._tooltipContainer);
+    }
+
+    _hideTooltip() {
+        this.removeChild(this._tooltipContainer);
     }
 }
 
