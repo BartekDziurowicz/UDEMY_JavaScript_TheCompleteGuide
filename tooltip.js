dc1652a37fccb825e4ac34689206343e751bdb5c
BartekDziurowicz <bartosz.dziurowicz@gmail.com>
2023-04-18 15:47:12 +0200

    observing attribute changes

--- tooltip.js
+++ tooltip.js
@@ -59,6 +59,14 @@ class Tooltip extends HTMLElement {
         this.style.position = 'relative';
     }
 
+    attributeChangedCallback(name, oldValue, newValue) {
+
+    }
+
+    static get observedAttributes() {
+        return ['text', 'class'];
+    }
+
     _showTooltip() {
         this._tooltipContainer = document.createElement('div');
         this._tooltipContainer.textContent = this._tooltipText;
